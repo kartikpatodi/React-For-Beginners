2d6920c30b79376c2d2944e7dfc3c7cd426cfe69
Kartik Patodi <patodi.kartik@gmail.com>
2018-03-02 05:00:52 +0530

    some components created

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+
+
+class App extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            fishes: {},
+            order: {}
+        };
+    }
+
+    addFish(fish){
+        
+    }
+
+    render(){
+        return (
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header tagline="Fresh Seafood Market"/>
+                </div>
+                <Order/>
+                <Inventory/>
+            </div>
+        )
+    }
+}
+
+export default App;
